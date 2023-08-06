7fcd2cbabd8b0045e2edf92188e3accaa9c6dea2
David Bradford <davidbradforduko@yahoo.com>
2023-08-06 08:06:04 +0100

    name is edited

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
        <header className="portfolio">
            <div className="container header__container">
                <h5>Hello I'm</h5>
-               <h1>David Bradford</h1>
+               <h1>David Uko Bradford</h1>
                <h5 className="text-light">MERN Stack(Front-End) Developer </h5>
                 <CTA />
                 <HeaderSocial />
