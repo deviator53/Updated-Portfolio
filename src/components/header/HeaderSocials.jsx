a65bbfb93218527f614f6accb317b5c21fc10337
David Bradford <davidbradforduko@yahoo.com>
2026-07-30 15:34:04 +0100

    fix portfolio update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa';
-import {BsTwitter} from 'react-icons/bs';
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/david-bradford-651769160/" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/deviator53" target="_blank"><FaGithub /></a>
-        <a href="https://twitter.com/Brad_for_d" target="_blank"><BsTwitter /></a>
-
+      <a
+        href="https://www.linkedin.com/in/david-bradford-651769160/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/deviator53" target="_blank" rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://twitter.com/Brad_for_d" target="_blank" rel="noreferrer">
+        <BsTwitter />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
