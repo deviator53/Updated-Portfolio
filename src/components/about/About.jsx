42d69b50269534a1e5b8ae472912665519f6c896
David Bradford <davidbradforduko@yahoo.com>
2026-07-30 15:41:51 +0100

    fix update issue

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,53 +1,56 @@
-import React from 'react';
-import './about.css';
-import ME from '../../assets/me-about.jpg';
-import PROFILE from '../../assets/david-bradford.png';
-import {FaAward} from 'react-icons/fa';
-import {FiUsers} from 'react-icons/fi';
-import {VscFolderLibrary} from 'react-icons/vsc';
+import React from "react";
+import "./about.css";
+import PROFILE from "../../assets/david-bradford.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-    return (
-        <section id="about">
-            <h5>Get To Know</h5>
-            <h2>About Me</h2>
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-            <div className="container about__container">
-                <div className="about__me">
-                    <div className="about__me-image">
-                        <img src={PROFILE} alt="about-me" />
-                    </div>
-                </div>
-                <div className="about__content">
-                    <div className="about__cards">
-                        <article className="about__card">
-                            <FaAward className="about__icon"/>
-                            <h5>Experience</h5>
-                            <small>4+ Years Working</small>
-                        </article>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={PROFILE} alt="about-me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>4+ Years Working</small>
+            </article>
 
-                        <article className="about__card">
-                            <FiUsers className="about__icon"/>
-                            <h5>Clients</h5>
-                            <small>200+ Worldwide</small>
-                        </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
 
-                        <article className="about__card">
-                            <VscFolderLibrary className="about__icon"/>
-                            <h5>Projects</h5>
-                            <small>80+ Completed</small>
-                        </article>
-                    </div>
-                    <p>David is a freelancer and a MERN Stack developer based in Abuja,
-                         Nigeria with a passion for building/developing software and web
-                          solutions. He has a knack for all things launching products, from
-                           planning and designing all the way to solving real-life problems
-                            with code. When not online, he loves watching football.</p>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            David is a freelancer and a MERN Stack developer based in Abuja,
+            Nigeria with a passion for building/developing software and web
+            solutions. He has a knack for all things launching products, from
+            planning and designing all the way to solving real-life problems
+            with code. When not online, he loves watching football.
+          </p>
 
-                            <a href="#contact" className="btn btn-primary">Let's Talk</a>
-                </div>
-            </div>
-        </section>
-    );
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default About;
